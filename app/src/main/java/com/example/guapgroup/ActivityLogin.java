5f6f8211ee8fab42a730cfbee54be3aa399cebe0
Мальцева Е.И <katenka_malceva@mail.ru>
2020-12-16 21:40:59 +0300

    Подключили БД, Дтологовое окно, Стартовый экран

--- app/src/main/java/com/example/guapgroup/ActivityLogin.java
+++ app/src/main/java/com/example/guapgroup/ActivityLogin.java
@@ -1,14 +1,102 @@
 package com.example.guapgroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class ActivityLogin extends AppCompatActivity {
+    Button btnSignin;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
+    RelativeLayout root;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignin = findViewById(R.id.btnSignin);
+        //root = findViewById(R.id.root_element);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnSignin.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                showSigninWindow();
+            }
+        });
+
+    }
+    private void showSigninWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Войти");
+        dialog.setMessage("Введите данные для входа");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View SigninWindow = inflater.inflate(R.layout.sing_in_window,null);
+        dialog.setView(SigninWindow);
+
+        final MaterialEditText email = SigninWindow.findViewById(R.id.email);
+        final MaterialEditText password = SigninWindow.findViewById(R.id.password);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Введите почту", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().length() < 5) {
+                    Snackbar.make(root, "Введите корректный пароль", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Intent intent = new Intent(ActivityLogin.this, MainActivity.class);
+                                startActivity(intent);
+
+                                //finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root, "Ошибка авторизации", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        dialog.show();
     }
 }
